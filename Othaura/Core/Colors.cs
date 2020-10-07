deb339786245d8964fcbb599a0a314065350d2e3
arcath- <arcath@gmail.com>
2020-10-07 08:16:28 -0600

    Initial map generation

---
+++ Othaura/Core/Colors.cs
@@ -0,0 +1,18 @@
+﻿using RLNET;
+
+namespace Othaura.Core {
+    public class Colors {
+        public static RLColor FloorBackground = RLColor.Black;
+        public static RLColor Floor = Palette.AlternateDarkest;
+        public static RLColor FloorBackgroundFov = Palette.DbDark;
+        public static RLColor FloorFov = Palette.Alternate;
+
+        public static RLColor WallBackground = Palette.SecondaryDarkest;
+        public static RLColor Wall = Palette.Secondary;
+        public static RLColor WallBackgroundFov = Palette.SecondaryDarker;
+        public static RLColor WallFov = Palette.SecondaryLighter;
+
+        public static RLColor TextHeading = Palette.DbLight;
+    }
+}
+
