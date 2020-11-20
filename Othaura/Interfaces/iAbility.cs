a493078f765dbafc80096a3b6bc7625ba966aaab
Shawn Thompson <arcath@gmail.com>
2020-11-20 15:13:55 -0700

    Working on including full tutorial changes into base.

---
+++ Othaura/Interfaces/iAbility.cs
@@ -0,0 +1,12 @@
+﻿namespace Othaura.Interfaces {
+
+    public interface IAbility {
+
+        string Name { get; }
+        int TurnsToRefresh { get; }
+        int TurnsUntilRefreshed { get; }
+
+        bool Perform();
+        void Tick();
+    }
+}
